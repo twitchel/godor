41ed1f37b8d55ace38495e4e5db29c922ae4d7f9
Daniel Jones <me@danielkjones.com>
2019-11-23 21:09:35 +1100

    MVP with emulation
    - Write file called 'open' to open door, rm file to close door
    - pushing to MQTT

--- main.go
+++ main.go
@@ -2,23 +2,109 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
+type configMqtt struct {
+	server string
+	topic  string
+}
+
+type configSensor struct {
+	pin       int
+	checkRate time.Duration
+}
+
+type configButton struct {
+	pin int
+}
+
+type config struct {
+	emulate bool
+	mqtt    *configMqtt
+	sensor  *configSensor
+	button  *configButton
+}
+
+type ping struct {
+	isOpen bool
+	time   time.Time
+}
+
 func main() {
-	server, topic := prepareConfig()
-	log.Println(topic)
-	_, error := connectMqtt(server)
+	c := prepareConfig()
+	log.Println(c.mqtt.topic)
+	client, error := connectMqtt(c.mqtt.server)
 
 	if error != nil {
 		panic(error)
 	}
+
+	pings := make(chan ping)
+
+	go func(c *config, p chan ping) {
+		// every 100 ms check if the pin is high or low and report this to then pings channel
+		ticker := time.NewTicker(c.sensor.checkRate)
+
+		for _ = range ticker.C {
+			if c.emulate {
+
+				_, err := ioutil.ReadFile("./open")
+
+				var isOpen bool
+				if err != nil {
+					isOpen = false
+				} else {
+					isOpen = true
+				}
+
+				pings <- ping{
+					isOpen: isOpen,
+					time:   time.Now(),
+				}
+			} else {
+				// REAL PIN!!!!!
+				pings <- ping{
+					isOpen: true,
+					time:   time.Now(),
+				}
+			}
+
+		}
+	}(c, pings)
+
+	recheck := false
+	recheckValue := true
+	var previousPing ping
+	for p := range pings {
+		if p.isOpen != previousPing.isOpen {
+			recheckValue = p.isOpen
+			recheck = true
+		} else if recheck == true {
+			if p.isOpen == recheckValue {
+
+				payload := fmt.Sprintf("{\"isOpen\":%v}", p.isOpen)
+				go client.Publish(c.mqtt.topic, byte(0), false, payload)
+
+				log.Printf("Fire: %v", p.isOpen)
+			}
+
+			recheck = false
+		}
+
+		previousPing = p
+	}
+
 }
 
-func prepareConfig() (string, string) {
+func prepareConfig() *config {
+
 	mqttServer, ok := os.LookupEnv("MQTT_SERVER")
 
 	if !ok {
@@ -33,7 +119,22 @@ func prepareConfig() (string, string) {
 
 	log.Println(mqttTopic)
 
-	return mqttServer, mqttTopic
+	return &config{
+		emulate: true,
+		mqtt: &configMqtt{
+			server: mqttServer,
+			topic:  mqttTopic,
+		},
+
+		sensor: &configSensor{
+			pin:       1,
+			checkRate: time.Second,
+		},
+
+		button: &configButton{
+			pin: 1,
+		},
+	}
 }
 
 func connectMqtt(server string) (MQTT.Client, error) {
