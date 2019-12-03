5f70a2009d36c8e52366aedddef1322decfc70ff
Daniel Jones <me@danielkjones.com>
2019-12-03 21:26:19 +1000

    Fix broken test

--- config_test.go
+++ config_test.go
@@ -9,6 +9,6 @@ import (
 func Test_prepareConfig(t *testing.T) {
 	c := prepareConfig()
 	assert.EqualValues(t, "tcp://192.168.1.194:1883", c.mqtt.server)
-	assert.EqualValues(t, "1", c.sensor.pin)
+	assert.EqualValues(t, "GPIO22", c.sensor.pin)
 	assert.EqualValues(t, "godor/door1/state", c.sensor.topic)
 }
